411c17299c328fbfbe08392dc62649b5882476cc
andrey <smooker14@gmail.com>
2020-11-05 17:25:06 +0200

    commit

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,11 +7,13 @@ import { LoginPageComponent } from './containers/login-page/login-page.component
 import { InputModule } from '@shared/directives/input/input.module';
 import { QuicklinkModule } from 'ngx-quicklink';
 import { AuthLayoutModule } from '../shared/layout/auth-layout/auth-layout.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginPageComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AuthRoutingModule,
     ButtonModule,
     InputModule,
--- src/app/auth/containers/login-page/login-page.component.ts
+++ src/app/auth/containers/login-page/login-page.component.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ReplayControlValueChanges } from '@helpers/replay-control-value-changes';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'my-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
-  public formControl: FormControl = new FormControl();
+export class LoginPageComponent implements OnInit, OnDestroy {
   public form: FormGroup;
+  private subs: Subscription[] = [];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
-    const value$ = new ReplayControlValueChanges(this.formControl).subscribe();
+    this.setForm();
+
+    this.subs.push(new ReplayControlValueChanges(this.nameControl).subscribe(value => {
+      console.log(value);
+    }));
+  }
+
+  public ngOnDestroy(): void {
+    this.subs.forEach(sub => {
+      sub.unsubscribe();
+    });
+  }
+
+  public get nameControl(): FormControl {
+    return this.form.get('name') as FormControl;
+  }
+
+  public onSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.router.navigate(['/users']);
+  }
+
+  private setForm(): void {
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.maxLength(6)]],
+    });
   }
 
 }
