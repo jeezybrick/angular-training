70a6beafff95aac27331881830d66b6f9108f5cf
andrey <smooker14@gmail.com>
2020-11-06 18:45:45 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { QuicklinkModule } from 'ngx-quicklink';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     CoreModule,
     QuicklinkModule
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,11 @@
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ErrorHandler, NgModule, Optional, SkipSelf } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
 import { httpInterceptorProviders } from '@core/interceptors';
 
 // core providers vs providedIn: 'root'
 // https://stackoverflow.com/questions/50860898/angular-6-services-providedin-root-vs-coremodule
 @NgModule({
-  imports: [BrowserModule, RouterModule, HttpClientModule],
-  exports: [BrowserModule, HttpClientModule],
+  imports: [RouterModule],
   providers: [
     httpInterceptorProviders
   ],
