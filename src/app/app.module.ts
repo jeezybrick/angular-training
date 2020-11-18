92ff478f80d5111fb40db9e626a6ef6549615d19
andrey <smooker14@gmail.com>
2020-11-18 16:00:23 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { QuicklinkModule } from 'ngx-quicklink';
 import { HttpClientModule } from '@angular/common/http';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, CoreModule, QuicklinkModule],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule, CoreModule, QuicklinkModule],
+  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { floatLabel: 'always', appearance: 'standard' } }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -8,9 +8,21 @@ import { ButtonModule } from '../shared/components/button/button.module';
 import { InputModule } from '../shared/directives/input/input.module';
 import { MainLayoutModule } from '../shared/layout/main-layout/main-layout.module';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [DemoComponent],
-  imports: [CommonModule, DemoRoutingModule, QuicklinkModule, ButtonModule, InputModule, MainLayoutModule, MatIconModule],
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    QuicklinkModule,
+    ButtonModule,
+    InputModule,
+    MainLayoutModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class DemoModule {}
